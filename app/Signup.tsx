2fe78f9c2f78d67f2a217852fa840a2ad04bd93d
Shironium09 <dustinj.bal09@gmail.com>
2026-05-03 16:19:59 +0800

    feat(login/signup): added login and signup pages ui

--- app/Signup.tsx
+++ app/Signup.tsx
@@ -1,27 +1,77 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {useRouter} from 'expo-router';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import { useRouter } from 'expo-router';
+import Logo from '../assets/logo.png';
 
-export default function SignupScreen(){
+export default function SignupScreen() {
 
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <View>
-      <Text>Signup Screen</Text>
-      <TouchableOpacity
-        style={{borderWidth: 1}}
-        onPress={() => router.push('/Login')}
-      >
-        <Text>Go to Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{borderWidth: 1}}
-        onPress={() => router.replace('/(tabs)/home')}
-      >
-        <Text>Go to Home (Signup)</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    return (
+        <View className="flex justify-center bg-stone-100 h-screen py-20">
+            <View className="h-full w-full flex flex-col justify-evenly gap-20">
+                <View>
+                    <Image source={Logo} className="w-40 h-40 mx-auto" />
+                    <Text className="text-center text-5xl font-bold">Leftover</Text>
+                    <Text className="text-center text-xl">Welcome back! Hope you're ready to cook!</Text>
+                </View>
+                <View className="flex flex-col gap-3 justify-center items-center px-10">
+                    <TextInput
+                        placeholder="Email"
+                        onChangeText={(e) => { setEmail(e) }}
+                        autoComplete='email'
+                        className="border-2 p-2 rounded-full w-full text-2xl text-left px-5 gap-3"
+                    />
+                    <View className="flex flex-row justify-between w-full">
+                        <TextInput
+                            placeholder="First Name"
+                            onChangeText={(e) => { setFirstName(e) }}
+                            className="border-2 p-2 rounded-full w-[50%] text-2xl text-left px-5"
+                        />
+                        <TextInput
+                            placeholder="Last Name"
+                            onChangeText={(e) => { setLastName(e) }}
+                            className="border-2 p-2 rounded-full w-[50%] text-2xl text-left px-5"
+                        />
+                    </View>
+                    <TextInput
+                        placeholder="Password"
+                        onChangeText={(e) => { setPassword(e) }}
+                        secureTextEntry={true}
+                        className="border-2 p-2 rounded-full w-full text-2xl text-left px-5"
+                    />
+                    <TextInput
+                        placeholder="Confirm Password"
+                        onChangeText={(e) => { setConfirmPass(e) }}
+                        secureTextEntry={true}
+                        className="border-2 p-2 rounded-full w-full text-2xl text-left px-5"
+                    />
+                </View>
+                <View className="flex flex-col gap-5 justify-center items-center">
+                    <TouchableOpacity
+                        className='bg-emerald-700 rounded-full px-6 py-4'
+                        onPress={() => router.replace('/(tabs)/home')}
+                    >
+                        <Text className='text-white text-2xl font-bold w-80 text-center'>Login</Text>
+                    </TouchableOpacity>
+                    <View className="flex flex-row gap-2">
+                        <Text className='text-center text-xl'>Don't have an account?</Text>
+                        <TouchableOpacity
+                            onPress={() => router.push('/Signup')}
+                        >
+                            <Text className="text-center text-xl text-emerald-700 font-semibold underline underline-offset-4">Signup!</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
+
+    );
 
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {useRouter} from 'expo-router';
+
+import Logo from '../assets/logo.png';
 
 export default function IndexScreen(){
 
   const router = useRouter();
 
   return (
-    <View>
-      <Text>Welcome to Leftover</Text>
-      <TouchableOpacity
-        style={{borderWidth: 1}}
-        onPress={() => router.push('/Login')}
-      >
-        <Text>Go to Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{borderWidth: 1}}
-        onPress={() => router.push('/Signup')}
-      >
-        <Text>Go to Signup</Text>
-      </TouchableOpacity>
+    <View className="flex justify-center bg-stone-100 h-screen py-20">
+        <View className="flex flex-col justify-between h-full py-20">
+            <View>
+                <Image source={Logo} className="w-40 h-40 mx-auto" />
+                <Text className="text-center text-5xl font-bold">Leftover</Text>
+                <Text className="text-center text-xl">Turn Your Leftovers Into Delicious Meals!</Text>
+            </View>
+            <View className="flex flex-col gap-5 justify-center items-center">
+                <TouchableOpacity
+                    className='bg-emerald-700 rounded-full px-6 py-4'
+                    onPress={() => router.push('/Login')}
+                >
+                    <Text className='text-white text-2xl font-bold w-80 text-center'>Login Your Account</Text>
+                </TouchableOpacity>
+                <View className="flex flex-row gap-2">                   
+                    <Text className='text-center text-xl'>Don't have an account?</Text>
+                    <TouchableOpacity                        
+                        onPress={() => router.push('/Signup')}
+                    >
+                        <Text className="text-center text-xl text-emerald-700 font-semibold underline underline-offset-4">Signup!</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
     </View>
   );
 
